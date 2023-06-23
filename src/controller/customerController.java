435b29ad2f9f03c6cdb81a756ab7fcfb86a1dc9b
BINITA88 <122206547+BINITA88@users.noreply.github.com>
2023-06-23 07:49:00 +0545

    first commit
    i am creating controller file for customerView

---
+++ src/controller/customerController.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author User
+ */
+public class customerController {
+    
+    public void function1(){
+        
+    }
+    
+}
