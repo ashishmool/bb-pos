fb0e3584b41bfb79ce680128eb9de9d5eadeeed8
Sujan <sujansapkota789@gmail.com>
2023-07-04 15:48:15 +0545

    supplierControllerAdd

---
+++ src/controller/supplierControllerAdd.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class supplierControllerAdd {
+    
+    public static boolean addSupplier(supplierModel supplier){
+        try{
+            Connection connection = myConnection.myDatabase();
+            PreparedStatement pst = connection.prepareStatement("INSERT INTO supplier VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+            pst.setObject(1,supplier.getSupplier_id());
+            pst.setObject(2,supplier.getSupplier_name());
+            pst.setObject(3,supplier.getSupplier_vatpan());
+            pst.setObject(4,supplier.getSupplier_address());
+            pst.setObject(5,supplier.getSupplier_city());
+            pst.setObject(6,supplier.getSupplier_email());
+            pst.setObject(7,supplier.getSupplier_cname());
+            pst.setObject(8,supplier.getSupplier_cdesig());
+            pst.setObject(9,supplier.getSupplier_cppan());
+            pst.setObject(10,supplier.getSupplier_cemail());
+            pst.setObject(11,supplier.getSupplier_cmob());
+            
+            int result = pst.executeUpdate();
+            return(result>0);
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        return false;
+    }
+}
