20a5c2c9b9b67c7a27ee86abe7647da0b5b1f8d5
Sujan <sujansapkota789@gmail.com>
2023-07-04 16:11:37 +0545

    supplierControllerDelete

--- src/controller/supplierController.java
+++ src/controller/supplierController.java
@@ -94,6 +94,21 @@ public class supplierController {
         }
         return false;
     }
+
+    public static boolean deleteSupplier(supplierModel supplier){
+        try{
+            Connection connection = myConnection.myDatabase();
+            String sql = "DELETE FROM supplier WHERE s_id = ?";
+            PreparedStatement pst = connection.prepareStatement(sql);
+            pst.setObject(1, supplier.getSupplier_id());
+            int result = pst.executeUpdate();
+            return (result>0);
+        }
+        catch (SQLException e3){
+                System.out.println(e3.getMessage());
+            }
+            return false;
+    }
     
 
 
